cf1e76263167828ebd0ce18bc20ed9329ef852cf
dxshim90 <dxshim90@gmail.com>
2019-09-16 14:49:35 +0100

    Seach Feature Working

--- src/Components/Coins/cryptoCoin.js
+++ src/Components/Coins/cryptoCoin.js
@@ -2,24 +2,33 @@ import React from "react";
 import Container from "./container";
 import "./crypto.css";
 
-const CryptoCoin = (props) => {
-    const { cryptos } = props
-    const link = `https://www.cryptocompare.com${cryptos.CoinInfo.ImageUrl}`
-    const priceChange = cryptos.DISPLAY.GBP.CHANGE24HOUR.split(" ")
-    const int = Number(priceChange[1])
-    return (
-        <div>
-         <div className="flexm">
-          <Container>
-          <img className="coin-image" src={link} alt={cryptos.CoinInfo.Name} width="100px" />
+const CryptoCoin = props => {
+  const { cryptos } = props;
+  const link = `https://www.cryptocompare.com${cryptos.CoinInfo.ImageUrl}`;
+  const priceChange = cryptos.DISPLAY.GBP.CHANGE24HOUR.split(" ");
+  const int = Number(priceChange[1]);
+  return (
+    <div>
+      <div className="flexm">
+        <Container>
+          <img
+            className="coin-image"
+            src={link}
+            alt={cryptos.CoinInfo.Name}
+            width="100px"
+          />
           <h2 className="card-title">{cryptos.CoinInfo.Name}</h2>
           <h2 className="card-title">Price: {cryptos.DISPLAY.GBP.PRICE}</h2>
-          <h2 className="card-title" style={int > 0 ? { color:'green'} : {color: 'red'} }>Change: {cryptos.DISPLAY.GBP.CHANGE24HOUR}</h2>
-          </Container>
+          <h2
+            className="card-title"
+            style={int > 0 ? { color: "green" } : { color: "red" }}
+          >
+            Change: {cryptos.DISPLAY.GBP.CHANGE24HOUR}
+          </h2>
+        </Container>
       </div>
-      </div>
-    );
-  };
-
+    </div>
+  );
+};
 
 export default CryptoCoin;
