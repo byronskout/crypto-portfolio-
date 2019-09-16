b36546f015a5c19ada5a67da163266c7c616ca75
byronskout <vyskoutaris@hotmail.com>
2019-09-16 18:17:35 +0100

    Home routing setup

--- src/App.js
+++ src/App.js
@@ -1,21 +1,25 @@
 import React from "react";
 import "./App.css";
 
-import CryptoCoinList from "./Components/Coins/cryptoCoinList";
-import NewsContainer from "./Components/NewsContainer/NewsContainer";
 import Header from "./Components/Header/Header";
 import Footer from "./Components/Footer/Footer";
+import Home from './Homepage/Home';
+
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 class App extends React.Component {
   render() {
     return( 
       <div className="App">
         <Header />
-        <CryptoCoinList />
-        <NewsContainer />
+        <Router>
+          <Switch>
+        <Route exact path="/" component={Home}/>
+        </Switch>
+        </Router>
         <Footer />
       </div>
-    )}
-}
+    )};
+};
 
 export default App;
