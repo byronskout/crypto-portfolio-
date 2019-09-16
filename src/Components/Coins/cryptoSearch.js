b366cacdfcf42043dc9fba0a6ee52e1f8903ed37
byronskout <vyskoutaris@hotmail.com>
2019-09-16 22:33:52 +0100

    Coin Pages added

---
+++ src/Components/Coins/cryptoSearch.js
@@ -0,0 +1,26 @@
+import React from "react";
+import Container from "./container";
+import "./crypto.css";
+import { Link } from "react-router-dom";
+
+const CryptoSearch = (props) => {
+    const { searchResults } = props
+    const link = `https://www.cryptocompare.com${searchResults.ImageUrl}`
+    return (
+      <Link style={{ textDecoration: 'none' }} 
+      to={{
+        pathname: `/searched/${searchResults.Id}`,
+        data: searchResults
+      }}
+    >
+      <div className="flexm">
+        <Container>
+        <img className="coin-image" src={link} alt={searchResults.link} width="70px" />
+        <h3 className="card-title">{searchResults.CoinName}</h3>
+        </Container>
+        </div>
+        </Link>
+  );
+};
+
+export default CryptoSearch;
---
+++ src/Components/Footer/Footer.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./Footer.css"
+
+const Footer = () => (
+  <div className="footer">  
+  <ul>
+    <li><a href="#contact">Contact Us</a></li>
+    <li><a href="#news">About Us</a></li>
+    <li><a href="#news">About Us</a></li>
+    <li><a href="#news">About Us</a></li>
+    <li><a href="#news">About Us</a></li>
+  </ul>
+  </div>
+  );
+
+export default Footer;
